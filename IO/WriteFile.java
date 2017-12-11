707171efd94bbcaf754538987a9303efd6d87c3b
ChristianTaidi <32304230+ChristianTaidi@users.noreply.github.com>
2017-12-11 12:04:16 +0100

    Add files via upload

--- IO/WriteFile.java
+++ IO/WriteFile.java
@@ -3,17 +3,25 @@ package IO;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class WriteFile implements IOStream {
     FileOutputStream stream;
+    ObjectOutputStream objectOut;
 
     @Override
     public void abrir(File fich) throws IOException{
         stream=new FileOutputStream(fich);
+        objectOut = new ObjectOutputStream(stream);
+
+    }
+
+    public void write(Entity ent){
+        objectOut.writeObject(ent);
     }
 
     @Override
     public void cerrar(File fich) throws IOException{
-        stream.close();
+       objectOut.close();
     }
 }
